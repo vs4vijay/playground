cbff1fb603666c7089697abb4dfabd2ea1496d8f
vs4vijay <vs4vijay@gmail.com>
2023-10-10 00:30:30 +0530

    add rust

---
+++ rust/second_rocket/src/main.rs
@@ -0,0 +1,13 @@
+
+
+#[macro_use] extern crate rocket;
+
+#[get("/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/hello", routes![hello])
+}
